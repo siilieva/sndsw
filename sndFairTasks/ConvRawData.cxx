2b41e8798e1f83f6beed7c22e088efcaa4f515bd
siilieva <simona.ilieva.ilieva@cern.ch>
2023-08-17 09:47:45 +0200

    BM is labelled 'veto' for the board_mapping in the testbeam, adjust system

--- sndFairTasks/ConvRawData.cxx
+++ sndFairTasks/ConvRawData.cxx
@@ -837,7 +837,6 @@ void ConvRawData::DetMapping(string Path)
          if ( Path.find("testbeam_23") == string::npos ) s = 0;
          else s = 3;
          tmp = x.second.substr(0, x.second.find("_"));
-         if ( tmp =="Veto" ) s = 0;
          if ( tmp =="US" ) s = 1;
          if ( tmp=="DS" ) s = 2;
          if ( slots[slot.first] == x.first )
