879fb4be347c7442890e3c6b1e5d4e9a69d02015
siilieva <simona.ilieva.ilieva@cern.ch>
2024-01-10 15:03:10 +0100

    Add the linkdef file required to build ROOT dictionaries.

---
+++ analysis/cuts/AnalysisCutsLinkDef.h
@@ -0,0 +1,29 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedef;
+#pragma link C++ nestedfunctions;
+
+#pragma link C++ namespace snd::analysis_cuts;
+#pragma link C++ defined_in namespace snd::analysis_cuts;
+
+#pragma link C++ class snd::analysis_cuts::avgDSFiducialCut+;
+#pragma link C++ class snd::analysis_cuts::avgSciFiFiducialCut+;
+#pragma link C++ class snd::analysis_cuts::baseCut+;
+#pragma link C++ class snd::analysis_cuts::DSActivityCut+;
+#pragma link C++ class snd::analysis_cuts::eventDeltatCut+;
+#pragma link C++ class snd::analysis_cuts::EventHeaderBaseCut+;
+#pragma link C++ class snd::analysis_cuts::minSciFiConsecutivePlanes+;
+#pragma link C++ class snd::analysis_cuts::minSciFiHits+;
+#pragma link C++ class snd::analysis_cuts::MuFilterBaseCut+;
+#pragma link C++ class snd::analysis_cuts::sciFiBaseCut+;
+#pragma link C++ class snd::analysis_cuts::sciFiStationCut+;
+#pragma link C++ class snd::analysis_cuts::USQDCCut+;
+#pragma link C++ class snd::analysis_cuts::vetoCut+;
+#pragma link C++ class snd::analysis_cuts::DSVetoCut+;
+
+#endif
