d8b44e228f28e0ad79c63b036af20c2c4a09fc88
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-01-19 12:19:39 +0100

    Fix iteration over DetectorId enum, update comments
    Fixes #193

--- shipdata/ShipDetectorList.h
+++ shipdata/ShipDetectorList.h
@@ -3,13 +3,13 @@
 // -------------------------------------------------------------------------
 
 
-/** Defines unique identifier for all FAIR detector systems **/
+/** Defines unique identifier for all SND@LHC detector systems **/
 
 #ifndef ShipDetectorList_H
 #define ShipDetectorList_H 1
 
-// kSTOPHERE is needed for iteration over the enum. All detectors have to be put before.
+// kEndOfList is needed for iteration over the enum. All detectors have to be put before.
  enum DetectorId {kEmulsionDet,kLHCScifi,kMuFilter, kVETO, kTimeDet, ktauRpc, ktauHpt, ktauTT, ktauTarget, kStraw, kecal, khcal, kMuon , kPreshower, kTRSTATION, kSplitCal, kBox1, kSpectrometer\
 			  , kPixelModules, kSciFi, kScintillator, kMufluxSpectrometer, kMuonTagger, kUpstreamTagger, kEndOfList};
-// last one for UpstreamTagger, then last five for muonflux and Charm measurement, first three for ship@lhc
+// First three for SND@LHC, others legacy from SHiP
 #endif
